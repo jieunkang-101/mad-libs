85adedfa2b376e96ea8febec07edf36c6f93d134
Student <example@example.com>
2020-01-21 09:32:08 -0800

    add arraying

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,28 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var noun1Input = $("input#noun1").val();
-    var pastTenseVerb1 = $("input#pastTenseVerb1").val();
-    var pastTenseVerb2 = $("input#pastTenseVerb2").val();
-    var typeOfPerson = $("input#typeOfPerson").val();
-    var pastTenseVerb3 = $("input#pastTenseVerb3").val();
-    var place = $("input#place").val();
-    var emotion = $("input#emotion").val();
+    var blanks = ["noun1Input", "pastTenseVerb1", "pastTenseVerb1", "typeOfPerson", "pastTenseVerb3", "place", "emotion"]
 
-    $(".noun1").text(noun1Input);
-    $(".pastTenseVerb1").text(pastTenseVerb1);
-    $(".pastTenseVerb2").text(pastTenseVerb2);
-    $(".typeOfPerson").text(typeOfPerson);
-    $(".pastTenseVerb3").text(pastTenseVerb3);
-    $(".place").text(place);
-    $(".emotion").text(emotion);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+
+
+    // var noun1Input = $("input#noun1").val();
+    // var pastTenseVerb1 = $("input#pastTenseVerb1").val();
+    // var pastTenseVerb2 = $("input#pastTenseVerb2").val();
+    // var typeOfPerson = $("input#typeOfPerson").val();
+    // var pastTenseVerb3 = $("input#pastTenseVerb3").val();
+    // var place = $("input#place").val();
+    // var emotion = $("input#emotion").val();
+
+    // $(".noun1").text(noun1Input);
+    // $(".pastTenseVerb1").text(pastTenseVerb1);
+    // $(".pastTenseVerb2").text(pastTenseVerb2);
+    // $(".typeOfPerson").text(typeOfPerson);
+    // $(".pastTenseVerb3").text(pastTenseVerb3);
+    // $(".place").text(place);
+    // $(".emotion").text(emotion);
 
     $("#story").show();
 
